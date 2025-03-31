2a0496d8f866f70a5e187ddadb2f691184ba983a
LucasCheer <cheroyonatan2@gmail.com>
2025-03-30 21:31:19 -0300

    Agrego base de modulo de ventas

---
+++ src/pages/Sales.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function Sales() {
+  return (
+    <>
+    <main className='main-mostrador'>
+        <section className='section-mostrador'>
+            <div className='container-head-mostrador'>
+                <h2 className='titulo-mostrador'>Mostrador</h2>
+            </div>
+            <div className='container-mostrador'>
+                <div className='producto'>Producto</div>
+                <div className='cantidad'>Cantidad</div>
+                <div className='precio'>Precio</div>
+                <div className='total'>Total</div>
+            </div>
+        </section>
+        <section className='section-inventario'>
+            
+        </section>
+    </main>
+    </>
+  )
+}
+
+export default Sales
