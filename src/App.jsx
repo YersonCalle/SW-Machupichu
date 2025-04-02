b91ebd688f37d2bcbbf2d8c63b762265e3ced884
LucasCheer <cheroyonatan2@gmail.com>
2025-04-02 19:29:22 -0300

    Agrego futuro navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import './App.css'
 import Login from './pages/Login.jsx'
+import Sales from './pages/Sales.jsx'
 
 function App() {
   return (
-    <>
-      <Login />
-    </>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/ventas/punto-de-venta" element={<Sales />} />
+    </Routes>
+  </BrowserRouter>
   )
 }
 
