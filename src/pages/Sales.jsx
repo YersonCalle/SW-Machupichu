9138a6108075595713410023e7e24311825d7721
Yerson <huaylliri.edgar10@gmail.com>
2025-04-14 00:18:54 -0300

    Interfaz punto de Ventas

--- src/pages/Sales.jsx
+++ src/pages/Sales.jsx
@@ -1,23 +1,34 @@
 import React from 'react'
+import '../styles/pages/Sales.css'
 
 function Sales() {
   return (
     <>
     <main className='main-mostrador'>
-        <section className='section-mostrador'>
-            <div className='container-head-mostrador'>
-                <h2 className='titulo-mostrador'>Mostrador</h2>
+        
+
+    <div class="sales-tittle">
+        <h1>MACHUPICCHU</h1>
+    </div>
+    <div class="sales-detail">
+        <div class="detail">
+            <h2>DETALLE</h2>
+        </div>
+        <div class="sales-products">
+            <h2>PRODUCTOS</h2>
+            <div class="products">
+                <div class="prod">Producto 1</div>
+                <div class="prod">Producto 2</div>
+                <div class="prod">Producto 3</div>
+                <div class="prod">Producto 4</div>
+                <div class="prod">Producto 5</div>
+                <div class="prod">Producto 6</div>
             </div>
-            <div className='container-mostrador'>
-                <div className='producto'>Producto</div>
-                <div className='cantidad'>Cantidad</div>
-                <div className='precio'>Precio</div>
-                <div className='total'>Total</div>
-            </div>
-        </section>
-        <section className='section-inventario'>
-            
-        </section>
+        </div>
+    </div>
+
+
+
     </main>
     </>
   )
