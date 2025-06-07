674c0572206746255dd9fb351f2f55fb9392b7ff
LucasCheer <cheroyonatan2@gmail.com>
2025-06-07 17:40:42 -0300

    Add view of products - New component CardProduct - New component Button1

---
+++ src/components/ui/CardProduct/CardProduct.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './CardProduct.css'
+import Button1 from '../Button1/Button1'
+
+
+function CardProduct({title, id, price, descrip}) {
+  return (
+    <div className='card-product'>
+        <div className='card-product-info'>
+            <h2 className='card-product-title'>{title}</h2>
+            <p className='card-product-id'>#{id}</p>
+            <p className='card-product-price'>${price}</p>
+            <p className='card-product-descrip'>{descrip}</p>
+        </div>
+        <div className='card-product-actions'>
+            <Button1 text='Actualizar' className='card-product-button'></Button1>
+            <Button1 text='Eliminar' className='card-product-button'></Button1>
+        </div>
+    </div>
+  )
+}
+
+export default CardProduct
--- src/pages/LoadPrducts.jsx
+++ src/pages/LoadPrducts.jsx
@@ -2,15 +2,14 @@ import React from 'react'
 import '../styles/pages/LoadProducts.css'
 import TitlePage from '../components/layout/TitlePage/TitlePage.jsx'
 import Button1 from '../components/ui/Button1/Button1.jsx'
-
+import CardProduct from '../components/ui/CardProduct/CardProduct.jsx'
 
 
 function LoadPrducts() {
 
-
   const AddProduct = (e) => {
     alert('Agregaste un producto')
-    }
+  }
   const DeleteProduct = (e) => {
     alert('Eliminaste un producto')
   }
@@ -21,10 +20,20 @@ function LoadPrducts() {
   return (
     <>
     <TitlePage title='Productos' />
+    <div className='list-products'>
+      <h3 className='subtitle'>Listado de productos</h3>
+      <CardProduct title='Pollo' id={20} price={500} descrip='Pollo a la brasa pe'/>
+      <CardProduct title='Chaufa' id={20} price={400} descrip='Chaufa mixto'/>
+      <CardProduct title='Lomo saltado' id={20} price={300} descrip='Lomo a lo pobre webon'/>
+      <CardProduct title='Chola de oro' id={20} price={200} descrip='Chola de tu hermana'/>
+      <CardProduct title='Salchipapa' id={20} price={250} descrip='Salchipapon'/>
+
+    </div>
     <div className='load-container'>
-        <Button1 text='Agregar producto' onClick={AddProduct}></Button1>
-        <Button1 text='Eliminar producto' onClick={DeleteProduct}></Button1>
-        <Button1 text='Actualizar producto' onClick={UpdateProduct}></Button1>
+        <h3 className='subtitle'>Eliminar Producto</h3>
+        <p>Precaución:</p>
+        <p>Tenga en cuenta que al ELIMINAR un producto se borrara COMPLETAMENTE del sistema, esto incluye si hay alguna mesa activa con este producto seleccionado. Se recomienda eliminar con cuidado y fuera de horario laboral,</p>
+        <Button1 text='ELIMINAR PRODUCTO' onClick={DeleteProduct}></Button1>
     </div>
     </>
     
