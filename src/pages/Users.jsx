5bd2007a400d7628b8b2f8e4047ac2e29786fd0a
Yerson <huaylliri.edgar10@gmail.com>
2025-06-14 18:50:23 -0300

    Add Users

---
+++ src/pages/Users.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import TitlePage from '../components/layout/TitlePage/TitlePage.jsx'
+import '../styles/pages/Users.css'
+
+function Users() {
+  return(
+    <>
+    <TitlePage title='Usuarios' />
+
+    
+    </>
+  )
+}
+
+export default Users
