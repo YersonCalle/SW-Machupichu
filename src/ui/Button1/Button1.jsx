503b55b44876d82d6df918db406a13f31f696951
LucasCheer <cheroyonatan2@gmail.com>
2026-01-01 17:13:47 -0300

    update: cambio el color por default del boton. - Se utiliza la variable css.

--- src/ui/Button1/Button1.jsx
+++ src/ui/Button1/Button1.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import './Button1.css'
 
 
-function Button1({text, onClick}) {
+function Button1({text, color, onClick}) {
   return (
-    <button onClick={onClick} className='button1'>
+    <button onClick={onClick} className='button1' style={{backgroundColor: color}}>
       {text}
     </button>
   )
